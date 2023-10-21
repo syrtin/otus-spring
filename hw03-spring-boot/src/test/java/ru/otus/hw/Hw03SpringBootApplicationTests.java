72413a3d54e0ad7f2c3c345d9babaca798e3906d
syrtin <sn23nov@gmail.com>
2023-10-21 16:35:43 +0300

    hw03-spring-boot empty template from spring.io

---
+++ hw03-spring-boot/src/test/java/ru/otus/hw/Hw03SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw03SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
