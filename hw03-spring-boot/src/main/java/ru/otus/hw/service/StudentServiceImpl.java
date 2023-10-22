cd5b2166cf295cdc9e7ca1d3db428d015f1754b1
syrtin <sn23nov@gmail.com>
2023-10-22 17:34:24 +0300

    hw03-spring-boot Encoding fail :(

--- hw03-spring-boot/src/main/java/ru/otus/hw/service/StudentServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/StudentServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.domain.Student;
 
@@ -10,10 +12,14 @@ public class StudentServiceImpl implements StudentService {
 
     private final IOService ioService;
 
+    private final MessageSource messageSource;
+
     @Override
     public Student determineCurrentStudent() {
-        var firstName = ioService.readStringWithPrompt("Please input your first name");
-        var lastName = ioService.readStringWithPrompt("Please input your last name");
+        var firstName = ioService.readStringWithPrompt(messageSource.getMessage("input.firstname", null,
+                LocaleContextHolder.getLocale()));
+        var lastName = ioService.readStringWithPrompt(messageSource.getMessage("input.lastname", null,
+                LocaleContextHolder.getLocale()));
         return new Student(firstName, lastName);
     }
 }
